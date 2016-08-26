90a2d7c49ed4a17e7e7b9a75f49e971d15ad9b40
root <root@d1stkfactory>
2016-08-25 21:39:06 -0400

    wo

---
+++ serve.js
@@ -0,0 +1,12 @@
+var spawn = require('child_process').spawn;
+
+var child = spawn('static-server', ['-p', '8001']);
+child.stdout.on('data', function(data) {
+    console.log('stdout: ' + data);
+});
+child.stderr.on('data', function(data) {
+    console.log('stdout: ' + data);
+});
+child.on('close', function(code) {
+    console.log('closing code: ' + code);
+});
